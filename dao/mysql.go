1673544e7461ae7d601168a05636f54c3a0d3054
tang-zhenhao <1098985413@qq.com>
2022-06-26 12:58:50 +0800

    first commit

---
+++ dao/mysql.go
@@ -0,0 +1,21 @@
+package dao
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func InitMySQL() (err error) {
+	dsn := "root:root1234@(127.0.0.1:13306)/bubble?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	fmt.Println("数据库连接成功")
+	return
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module bubble
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/gorm v1.23.6
+)
